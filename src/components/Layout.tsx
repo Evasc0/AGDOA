5fd5f2735aa29618d7c5bb9ea9b061d6911e0580
Evasc0 <145756607+Evasc0@users.noreply.github.com>
2025-04-09 12:13:28 +0800

    hehe

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = () => {
     <div className="min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white transition-colors duration-500 pb-16">
       {/* Header */}
       <header className="sticky top-0 z-40 bg-white dark:bg-gray-900 px-4 pt-4 pb-2 flex justify-between items-center shadow-sm">
-        <h1 className="text-lg font-bold">AGDOA Drivers</h1>
+        <h1 className="text-lg font-bold">AGDUWA Drivers</h1>
         <ThemeToggle />
       </header>
 
